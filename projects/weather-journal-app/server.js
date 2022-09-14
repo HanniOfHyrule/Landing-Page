c1c1b067cbbe5956e1fc2a142b01077c3aebe44f
Hanni Liebler <johanna.liebler@gmail.com>
2022-09-14 09:27:00 +0200

    finish the server functions and requests/fetch

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,5 +1,5 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 // Require Express to run server and routes
 const express = require("express");
@@ -9,14 +9,11 @@ const app = express();
 
 /* Dependencies */
 const bodyParser = require("body-parser");
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require("cors");
-// const { response } = require("express");
 app.use(cors());
 
 // Initialize the main project folder
@@ -24,31 +21,46 @@ app.use(express.static("website"));
 
 // Setup Server
 const port = 8080;
+
+//Confirm the server run
 const server = app.listen(port, listening);
 function listening() {
   console.log(server);
   console.log(`running on localhost: ${port}`);
 }
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Personal AP Key for OpenWeatherMap API
+// let process = {
+//   env: getData("/config"),
+// };
+
 //API Key request
-app.get("/config", (req, res) => {
-  res.send(
-    Object.fromEntries(
-      Object.entries(process.env).filter(([key]) =>
-        key.startsWith("WEATHERAPP_")
-      )
-    )
-  );
-});
+// app.get("/config", (req, res) => {
+//   res.send(
+//     Object.fromEntries(
+//       Object.entries(process.env).filter(([key]) =>
+//         key.startsWith("WEATHERAPP_")
+//       )
+//     )
+//   );
+// });
+
+// Setup empty JS object to hold the data
+projectData = [];
 
 //GET route
-app.get("/all", sendData);
-function sendData(req, res) {
-  res.send(projectData);
-  console.log(projectData);
-}
+// app.get("/all", sendData);
+// function sendData(req, res) {
+//   res.send(projectData);
+//   console.log(projectData);
+// }
 
-const addData = async (res, req) => {
+const addData = async (req, res) => {
+  console.log(req.body);
+
   let newInput = {
     zip: req.body.zip,
     thoughts: req.body.thoughts,
@@ -58,7 +70,7 @@ const addData = async (res, req) => {
   const currentWeather = await getWeather(newInput.zip);
   let entry = {
     date: getDate(),
-    temp: weather.main.temp,
+    temp: Math.round(currentWeather.main.temp),
     thoughts: newInput.thoughts,
   };
   projectData.unshift(entry);
@@ -68,14 +80,22 @@ const addData = async (res, req) => {
 
 app.post("/add", addData);
 
+/* Function to GET Project Data */
+function getDate() {
+  let d = new Date();
+  let newDate = d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
+  return newDate;
+}
+
 const getWeather = async (zip) => {
-  const baseURL = `https://pro.openweathermap.org/data/2.5/forecast/hourly?zip=${newZip}&appid=${process.env.WEATHERAPP_API_KEY}`;
-  const request = await fetch(baseURL + zip);
+  // const baseURL = `https://pro.openweathermap.org/data/2.5/forecast/hourly?zip=${newZip}&appid=${process.env.WEATHERAPP_API_KEY}`;
+  const baseURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},de&appid=${process.env.WEATHERAPP_API_KEY}&units=metric`;
+  const request = await fetch(baseURL);
   try {
     const weatherData = await request.json();
     console.log(weatherData);
     return weatherData;
   } catch (error) {
-    console.log("get error", error);
+    console.log("error about get the weather", error);
   }
 };
