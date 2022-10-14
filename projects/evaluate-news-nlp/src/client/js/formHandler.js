853c045d48edc72ace09ec4bd4d1a8ca815496be
Hanni Liebler <johanna.liebler@gmail.com>
2022-10-14 11:35:23 +0200

    API req and res with update UI

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,16 +1,21 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
+  // check what text was put into the form field
+  let formText = document.getElementById("name").value;
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+  Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+  console.log("::: Form Submitted :::");
+
+  fetch("/sentiment", {
+    method: "POST",
+    body: JSON.stringify({ text: formText }),
+  }).then(async function (res) {
+    const sentiment = await res.json();
+
+    document.getElementById("results").innerHTML = formText;
+    document.getElementById("sentiment").innerHTML = JSON.stringify(sentiment);
+  });
 }
 
-export { handleSubmit }
+export { handleSubmit };
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,23 +1,34 @@
 var path = require("path");
+const fetch = require("node-fetch");
 const express = require("express");
-const mockAPIResponse = require("./mockAPI.js");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+//My personal API KEY stored in an enviroment variable
 const dotenv = require("dotenv");
 dotenv.config();
-
-var textapi = new meaningAPI({
-  application_id: process.env.API_ID,
-  application_key: process.env.API_LICENCE_KEY,
-});
+const apiKey = process.env.API_LICENSE_KEY;
 
 const app = express();
-
 app.use(express.static("dist"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 console.log(__dirname);
 
+app.post("/sentiment", async function (req, res) {
+  const sentimentResponse = await analyseSentiment(req.body.text);
+  console.log(sentimentResponse);
+  res.json(sentimentResponse);
+});
+
 app.get("/", function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve("src/client/views/index.html"));
+  res.sendFile("dist/index.html");
 });
 
 // designates what port the app will listen to for incoming requests
@@ -25,6 +36,21 @@ app.listen(8080, function () {
   console.log("Example app listening on port 8080!");
 });
 
-app.get("/test", function (req, res) {
-  res.send(mockAPIResponse);
-});
+async function analyseSentiment(text) {
+  try {
+    const formdata = new URLSearchParams();
+    formdata.append("key", `${apiKey}`);
+    formdata.append("txt", text);
+    formdata.append("lang", "auto");
+
+    const response = await fetch("https://api.meaningcloud.com/sentiment-2.1", {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
+    });
+    const body = await response.json();
+    return body;
+  } catch (error) {
+    console.error(error);
+  }
+}
