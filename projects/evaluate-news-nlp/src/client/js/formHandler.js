9a0795c1c00c6c47dfe8bbff1e65cca951b11f8f
Hanni Liebler <johanna.liebler@gmail.com>
2022-10-17 14:57:45 +0200

    add elements and fix the response

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,20 +1,39 @@
 function handleSubmit(event) {
   event.preventDefault();
   // check what text was put into the form field
-  let formText = document.getElementById("name").value;
+  let formText = document.getElementById("url").value;
 
-  Client.checkForName(formText);
+  Client.checkForURL(formText);
 
-  console.log("::: Form Submitted :::");
+  //TODO: if - validierung URL
+
+  console.log(formText);
 
   fetch("/sentiment", {
     method: "POST",
     body: JSON.stringify({ text: formText }),
+    headers: {
+      "Content-Type": "application/json",
+    },
   }).then(async function (res) {
     const sentiment = await res.json();
+    console.log(sentiment);
 
-    document.getElementById("results").innerHTML = formText;
-    document.getElementById("sentiment").innerHTML = JSON.stringify(sentiment);
+    document.getElementById("results").innerHTML = JSON.stringify(
+      sentiment.sentence_list[14].text
+    );
+    document.getElementById("sentiment").innerHTML = JSON.stringify(
+      sentiment.subjectivity
+    );
+    document.getElementById("polarity").innerHTML = JSON.stringify(
+      sentiment.score_tag
+    );
+    document.getElementById("confidence").innerHTML = JSON.stringify(
+      sentiment.confidence
+    );
+    document.getElementById("irony").innerHTML = JSON.stringify(
+      sentiment.irony
+    );
   });
 }
 
