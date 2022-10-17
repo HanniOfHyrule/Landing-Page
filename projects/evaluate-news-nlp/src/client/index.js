79f6bf1ae32762889a47d6690e2053d94c216eff
Hanni Liebler <johanna.liebler@gmail.com>
2022-10-17 14:49:32 +0200

    add new folder and some styling

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,9 +1,10 @@
-import { checkForName } from "./js/nameChecker";
+import { checkForURL } from "./js/urlChecker";
 import { handleSubmit } from "./js/formHandler";
 import "./styles/resets.scss";
 import "./styles/base.scss";
 import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
+import "./styles/main.scss";
 
-export { checkForName, handleSubmit };
+export { checkForURL, handleSubmit };
