750040a3a9c983b35fc028fbbd4e188fdf90504e
Hanni Liebler <johanna.liebler@gmail.com>
2022-10-18 13:06:48 +0200

    refactoring for easyer testing

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,38 +1,43 @@
+function updateUiWithSentiment(sentiment) {
+  console.log(sentiment);
+
+  document.getElementById("results").innerHTML = JSON.stringify(
+    sentiment.agreement
+  );
+  document.getElementById("sentiment").innerHTML = JSON.stringify(
+    sentiment.subjectivity
+  );
+  document.getElementById("polarity").innerHTML = JSON.stringify(
+    sentiment.score_tag
+  );
+  document.getElementById("confidence").innerHTML = JSON.stringify(
+    sentiment.confidence
+  );
+  document.getElementById("irony").innerHTML = JSON.stringify(sentiment.irony);
+}
+
 function handleSubmit(event) {
-  event.preventDefault();
+  window.event.preventDefault();
+  let url = document.getElementById("url").value;
 
-  let formText = document.getElementById("url").value;
+  postContent(url);
+}
 
-  if (Client.checkForURL(formText)) {
+function postContent(url, callback) {
+  if (Client.isValidURL(url)) {
     fetch("/sentiment", {
       method: "POST",
-      body: JSON.stringify({ text: formText }),
+      body: JSON.stringify({ text: url }),
       headers: {
         "Content-Type": "application/json",
       },
     }).then(async function (res) {
       const sentiment = await res.json();
-      console.log(sentiment);
-
-      document.getElementById("results").innerHTML = JSON.stringify(
-        sentiment.sentence_list[14].text
-      );
-      document.getElementById("sentiment").innerHTML = JSON.stringify(
-        sentiment.subjectivity
-      );
-      document.getElementById("polarity").innerHTML = JSON.stringify(
-        sentiment.score_tag
-      );
-      document.getElementById("confidence").innerHTML = JSON.stringify(
-        sentiment.confidence
-      );
-      document.getElementById("irony").innerHTML = JSON.stringify(
-        sentiment.irony
-      );
+      callback ? callback(sentiment) : updateUiWithSentiment(sentiment);
     });
   } else {
     alert("please enter a vaild URL!");
   }
 }
 
-export { handleSubmit };
+export { handleSubmit, updateUiWithSentiment };
